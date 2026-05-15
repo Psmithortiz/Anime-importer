d9083f932073b39b1b514c7836f273f263497aa3
Pablo Smith <p.smith.ortiz@gmail.com>
2026-05-15 12:53:26 -0400

    Complete main pipeline with XML export

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from exporter import read_list
+from exporter import read_list, export_xml
 from mal_client import search_anime
 from resolver import resolve_title, select_from_results
 
@@ -6,5 +6,5 @@ acumulador = []
 titles = read_list("anime_list.txt")
 for title in titles:
     acumulador.append(select_from_results(search_anime(resolve_title(title)), title))
-
+export_xml(acumulador, "output.xml")
 print(acumulador)
